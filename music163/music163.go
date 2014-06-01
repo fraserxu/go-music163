bca8c3727276e76fafb7dee020f96edf5567d5b1
fraserxu <xvfeng123@gmail.com>
2014-06-01 22:52:03 +0800

    fix indent

--- music163/music163.go
+++ music163/music163.go
@@ -1,12 +1,12 @@
 package music163
 
 import (
-  "bytes"
+	"bytes"
 	"encoding/json"
-  "errors"
+	"errors"
 	"fmt"
-  "io"
-  "io/ioutil"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 	"reflect"
@@ -85,105 +85,105 @@ func NewClient(httpClient *http.Client) *Client {
 
 // NewRequest creates an API request.
 func (c *Client) NewRequest(method, urlStr string, body interface{}) (*http.Request, error) {
-  rel, err := url.Parse(urlStr)
-  if err != nil {
-    return nil, err
-  }
+	rel, err := url.Parse(urlStr)
+	if err != nil {
+		return nil, err
+	}
 
-  u := c.BaseURL.ResolveReference(rel)
+	u := c.BaseURL.ResolveReference(rel)
 
-  buf := new(bytes.Buffer)
-  if body != nil {
-    err := json.NewEncoder(buf).Encode(body)
-    if err != nil {
-      return nil, err
-    }
-  }
+	buf := new(bytes.Buffer)
+	if body != nil {
+		err := json.NewEncoder(buf).Encode(body)
+		if err != nil {
+			return nil, err
+		}
+	}
 
-  req, err := http.NewRequest(method, u.String(), buf)
-  if err != nil {
-    return nil, err
-  }
+	req, err := http.NewRequest(method, u.String(), buf)
+	if err != nil {
+		return nil, err
+	}
 
-  req.Header.Add('referer', referer)
-  req.Header.Add("User-Agent", c.UserAgent)
-  return req, nil
+	req.Header.Add("referer", referer)
+	req.Header.Add("User-Agent", c.UserAgent)
+	return req, nil
 }
 
 // Response is a API response.
 type Response struct {
-  *http.Response
+	*http.Response
 }
 
 // newResponse creates a new Response for the provided http.Response.
 func newResponse(r *http.Response) *Response {
-  response := &Response{Response: r}
-  return response
+	response := &Response{Response: r}
+	return response
 }
 
 // Do sends an API request and returns the API response.
 func (c *Client) Do(req *http.Request, v interface{}) (*Response, error) {
-  resp, err := c.client.Do(req)
-  if err != nil {
-    return nil, err
-  }
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
 
-  defer resp.Body.Close()
+	defer resp.Body.Close()
 
-  response := newResponse(resp)
+	response := newResponse(resp)
 
-  err = CheckResponse(resp)
-  if err != nil {
-    return response, err
-  }
+	err = CheckResponse(resp)
+	if err != nil {
+		return response, err
+	}
 
-  if v != nil {
-    if w, ok := v.(io.Write); ok {
-      io.Copy(w, resp.Body)
-    } else {
-      err = json.NewDecoder(resp.Body).Decode(v)
-    }
-  }
-  return response, err
+	if v != nil {
+		if w, ok := v.(io.Write); ok {
+			io.Copy(w, resp.Body)
+		} else {
+			err = json.NewDecoder(resp.Body).Decode(v)
+		}
+	}
+	return response, err
 }
 
 /*
 An ErrorResponse reports one or more errors caused by and API request.
- */
+*/
 type ErrorResponse struct {
-  Response *http.Response
-  Message string `json:"message"`
-  Errors []Error `json:"errors"`
+	Response *http.Response
+	Message  string  `json:"message"`
+	Errors   []Error `json:"errors"`
 }
 
 func (r *ErrorResponse) Error() string {
-  return fmt.Sprintf("%v %v: %d %v %+v",
-    r.Response.Request.Method, r.Response.Request.URL,
-    r.Response.StatusCode, r.Message, r.Errors)
+	return fmt.Sprintf("%v %v: %d %v %+v",
+		r.Response.Request.Method, r.Response.Request.URL,
+		r.Response.StatusCode, r.Message, r.Errors)
 }
 
 /*
 An Error reports more details on an individual error in an ErrorResponse.
- */
+*/
 type Error struct {
-  Resource string `json:"resource"`
-  Field string `json:"field"`
-  Code string `json:"code"`
+	Resource string `json:"resource"`
+	Field    string `json:"field"`
+	Code     string `json:"code"`
 }
 
 func (e *Error) Error() string {
-  return fmt.Sprintf("%v error caused by %v filed on %v resource", e.Code, e.Field, e.Resource)
+	return fmt.Sprintf("%v error caused by %v filed on %v resource", e.Code, e.Field, e.Resource)
 }
 
 //CheckResponse checks the API response for errors, and returns them if present.
 func CheckResponse(r *http.Response) error {
-  if c := r.StatusCode; 200 <= c && c<=299 {
-    return nil
-  }
-  errorResponse := &ErrorResponse{Response: r}
-  data, err := ioutil.ReadAll(r.Body)
-  if err == nil && data != nil {
-    json.Unmarshal(data, errorResponse)
-  }
-  return errorResponse
+	if c := r.StatusCode; 200 <= c && c <= 299 {
+		return nil
+	}
+	errorResponse := &ErrorResponse{Response: r}
+	data, err := ioutil.ReadAll(r.Body)
+	if err == nil && data != nil {
+		json.Unmarshal(data, errorResponse)
+	}
+	return errorResponse
 }
